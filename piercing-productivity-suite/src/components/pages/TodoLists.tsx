173f84ecd0e4f0cd616e493cf68871e0c8bc7e06
Dario Piotrowicz <dario@cloudflare.com>
2022-10-07 19:05:56 +0100

    implement lists editing

--- piercing-productivity-suite/src/components/pages/TodoLists.tsx
+++ piercing-productivity-suite/src/components/pages/TodoLists.tsx
@@ -3,7 +3,16 @@ import "./TodoLists.css";
 export function TodoLists() {
   return (
     <div className="todo-lists-page">
-      <piercing-fragment-outlet fragment-id="todo-lists" />
+      <piercing-fragment-outlet
+        fragment-id="todo-lists"
+        onTodoListClick={({
+          detail: { listName },
+        }: {
+          detail: { listName: string };
+        }) => {
+          alert(`${listName} list clicked`);
+        }}
+      />
       <piercing-fragment-outlet fragment-id="todos" />
     </div>
   );

d905ae59f9b8b8708dc6950876170bcb11057388
Dario Piotrowicz <dario@cloudflare.com>
2022-10-12 11:55:59 +0100

    rename piercing-lib to piercing-library

--- app/fragments/todo-lists/src/root.tsx
+++ app/fragments/todo-lists/src/root.tsx
@@ -6,7 +6,7 @@ import {
   useStore,
   useStylesScoped$,
 } from "@builder.io/qwik";
-import { dispatchPiercingEvent } from "piercing-lib";
+import { dispatchPiercingEvent } from "piercing-library";
 import { addTodoList, removeTodoList } from "./api";
 
 import styles from "./root.css?inline";

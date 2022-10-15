f9b252cd6681ceb5cab14633e1d5de1e6de18574
Dario Piotrowicz <dario@cloudflare.com>
2022-10-15 15:09:21 +0100

    add validation to login form

--- migration-strategies/piercing/app/fragments/login/src/root.tsx
+++ migration-strategies/piercing/app/fragments/login/src/root.tsx
@@ -1,4 +1,5 @@
 import {
+  $,
   component$,
   useRef,
   useStore,
@@ -9,17 +10,61 @@ import { dispatchPiercingEvent } from "piercing-library";
 import styles from "./root.css?inline";
 
 export const Root = component$(() => {
+  const possibleInputErrors = {
+    noSpaces: "noSpaces",
+    noSpecialCharacters: "noSpecialCharacters",
+    maxLength: "maxLength",
+    emptyInput: "emptyInput",
+  } as const;
+
+  const inputErrorMessages: Record<
+    typeof possibleInputErrors[keyof typeof possibleInputErrors],
+    string
+  > = {
+    [possibleInputErrors.noSpaces]: "Spaces are not allowed",
+    [possibleInputErrors.noSpecialCharacters]:
+      "Special characters are not allowed",
+    [possibleInputErrors.maxLength]:
+      "The provided username is too long, it needs not to be longer than 20 characters",
+    [possibleInputErrors.emptyInput]: "Please provide a username",
+  };
+
   const ref = useRef();
   useStylesScoped$(styles);
 
-  const inputsDetails = useStore<{ animatePasswordInput: boolean }>({
+  const inputsDetails = useStore<{
+    animatePasswordInput: boolean;
+    usernameInputError:
+      | null
+      | typeof possibleInputErrors[keyof typeof possibleInputErrors];
+    usernameInputTouched: boolean;
+  }>({
     animatePasswordInput: false,
+    usernameInputError: null,
+    usernameInputTouched: false,
   });
 
   const userData = useStore({
     username: "",
     password: "",
   });
+
+  const getUsernameInputError = $(
+    (
+      input: string
+    ): typeof possibleInputErrors[keyof typeof possibleInputErrors] | null => {
+      if (input.includes(" ")) return possibleInputErrors.noSpaces;
+      if (/[^a-zA-Z0-9]/.test(input))
+        return possibleInputErrors.noSpecialCharacters;
+      if (input.length > 20) {
+        return possibleInputErrors.maxLength;
+      }
+      if (inputsDetails.usernameInputTouched && !userData.username) {
+        return possibleInputErrors.emptyInput;
+      }
+      return null;
+    }
+  );
 
   return (
     <div class="root" ref={ref}>
@@ -39,8 +84,10 @@ export const Root = component$(() => {
           <label for="username-input">Username</label>
           <input
             id="username-input"
+            class={inputsDetails.usernameInputError ? "invalid" : ""}
             type="text"
-            onInput$={(event) => {
+            onFocus$={() => (inputsDetails.usernameInputTouched = true)}
+            onInput$={async (event) => {
               const value = (event.target as HTMLInputElement).value;
               userData.username = value;
               const usernameLength = value.length;
@@ -53,10 +100,23 @@ export const Root = component$(() => {
                 () => (inputsDetails.animatePasswordInput = false),
                 500
               );
+              inputsDetails.animatePasswordInput = true;
+              inputsDetails.usernameInputError = await getUsernameInputError(
+                value
+              );
+              setTimeout(
+                () => (inputsDetails.animatePasswordInput = false),
+                500
+              );
             }}
             value={userData.username}
             autoComplete="true"
           />
+          {inputsDetails.usernameInputError && (
+            <p class="input-error-message">
+              {inputErrorMessages[inputsDetails.usernameInputError]}
+            </p>
+          )}
         </div>
         <div class="form-control">
           <label for="password-input">Password</label>
@@ -70,7 +130,12 @@ export const Root = component$(() => {
               inputsDetails.animatePasswordInput ? "password-input-animate" : ""
             }
           />
-          <button class="submit-btn">Login</button>
+          <button
+            disabled={!userData.username || !!inputsDetails.usernameInputError}
+            class="submit-btn"
+          >
+            Login
+          </button>
         </div>
       </form>
     </div>

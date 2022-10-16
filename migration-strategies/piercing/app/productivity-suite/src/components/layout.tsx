bb8e69b250f114513a10e2fe8e5698478cd47426
Dario Piotrowicz <dario@cloudflare.com>
2022-10-16 13:15:00 +0100

    remove pages titles and sub-titles

--- migration-strategies/piercing/app/productivity-suite/src/components/layout.tsx
+++ migration-strategies/piercing/app/productivity-suite/src/components/layout.tsx
@@ -54,70 +54,17 @@ function Footer() {
   );
 }
 
-const todosPageTitles = {
-  title: "Todo Lists",
-  subTitle:
-    "Create and manage various lists of todos so that you never skip a bit.",
-};
-
-const pageTitlesMap: { [path: string]: { title: string; subTitle?: string } } =
-  {
-    "/todos": todosPageTitles,
-    "/": todosPageTitles,
-    "/calendar": {
-      title: "Calendar",
-      subTitle: "Check your calendar and organize meetings with your team.",
-    },
-    "/contacts": {
-      title: "Contacts",
-      subTitle: "View and manage the company contacts here.",
-    },
-    "/news": {
-      title: "News",
-      subTitle:
-        "Real all the latest company news, so that you can be always up to date.",
-    },
-  };
-
-function getPageTitle(path: string): string | undefined {
-  const pageTitleDetails = pageTitlesMap[path];
-  if (pageTitleDetails) return pageTitleDetails.title;
-
-  if (/\/todos\/[^/]?/.test(path)) {
-    return pageTitlesMap["/todos"].title;
-  }
-}
-
-function getPageSubTitle(path: string): string | undefined {
-  const pageTitleDetails = pageTitlesMap[path];
-  if (pageTitleDetails) return pageTitleDetails.subTitle;
-
-  if (/\/todos\/[^/]?/.test(path)) {
-    return pageTitlesMap["/todos"].subTitle;
-  }
-}
-
 export function Layout() {
   const location = useLocation();
 
   const path = location.pathname;
   const isLoginPage = path === "/login";
-  const pageTitle = getPageTitle(path);
-  const pageSubTitle = getPageSubTitle(path);
 
   return (
     <div className="layout">
       <Header />
       {!isLoginPage && <NavBar />}
       <main className="app-main">
-        {pageTitle && (
-          <div className="app-page-heading">
-            <h2 className="app-page-title">{pageTitle}</h2>
-            {pageSubTitle && (
-              <p className="app-page-sub-title">{pageSubTitle}</p>
-            )}
-          </div>
-        )}
         <Outlet />
       </main>
       <Footer />

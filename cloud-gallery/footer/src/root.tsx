82ca196e49a23b19501ac190be1bc74c1ad163fd
James Culveyhouse <jculveyhouse@cloudflare.com>
2022-10-19 13:24:18 -0500

    feat(cloud-gallery): add blog info to the footer

--- cloud-gallery/footer/src/root.tsx
+++ cloud-gallery/footer/src/root.tsx
@@ -4,5 +4,20 @@ import styles from "./Footer.css?inline";
 export const Footer = component$(() => {
   useStylesScoped$(styles);
 
-  return <div class="container">Copyright Cloudflare 2022</div>;
+  return (
+    <div class="container">
+      <div class="content">
+        <p>
+          This is a demo application for micro-frontends implemented using{" "}
+          <a href="https://workers.cloudflare.com/">Cloudflare Workers</a>. For
+          more information check out{" "}
+          <a href="https://blog.cloudflare.com/better-micro-frontends">
+            the accompanying blog post
+          </a>
+          .
+        </p>
+        <div class="copyright">Copyright Cloudflare 2022</div>
+      </div>
+    </div>
+  );
 });

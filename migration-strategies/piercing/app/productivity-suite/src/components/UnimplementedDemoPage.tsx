9bcdc07859e89202621b8cab848f27e9ab9c6de9
Dario Piotrowicz <dario@cloudflare.com>
2022-10-19 20:51:19 +0100

    fix bug in unimplemented page component

--- migration-strategies/piercing/app/productivity-suite/src/components/UnimplementedDemoPage.tsx
+++ migration-strategies/piercing/app/productivity-suite/src/components/UnimplementedDemoPage.tsx
@@ -18,11 +18,8 @@ export function UnimplementedDemoPage({
         <p>This page is not implemented as it is out of scope for this demo.</p>
         <p>
           To Interact with SSR fragments view the login form by{" "}
-          <button>logging out</button> or visit the{" "}
-          <Link to="/todos" onClick={auth.logout}>
-            todos page
-          </Link>
-          .
+          <button onClick={auth.logout}>logging out</button> or visit the{" "}
+          <Link to="/todos">todos page</Link>.
         </p>
       </div>
     </div>

fde347a70d44e8a27c2980baf26c66b07871ffa2
Dario Piotrowicz <dario@cloudflare.com>
2022-10-20 18:33:50 +0100

    refactor SelectedListCard component out of todo-lists

---
+++ migration-strategies/piercing/app/fragments/todo-lists/src/components/SelectedListCard.tsx
@@ -0,0 +1,117 @@
+import {
+  component$,
+  PropFunction,
+  useRef,
+  useStore,
+  useStylesScoped$,
+  useWatch$,
+} from "@builder.io/qwik";
+
+import styles from "./SelectedListCard.css?inline";
+
+export const SelectedListCard = component$(
+  ({
+    listName,
+    todoListsNames,
+    onEditListName$,
+    onDeleteList$,
+  }: {
+    listName: string;
+    todoListsNames: string[];
+    onEditListName$: PropFunction<(newName: string) => boolean>;
+    onDeleteList$: PropFunction<() => void>;
+  }) => {
+    useStylesScoped$(styles);
+
+    const newTodoInputState = useStore<{
+      editing: boolean;
+      value: string | null;
+      valid: boolean;
+      dirty: boolean;
+    }>({
+      editing: false,
+      value: null,
+      valid: false,
+      dirty: false,
+    });
+
+    const editRef = useRef<HTMLInputElement>();
+    editRef.current?.focus();
+
+    useWatch$(({ track }) => {
+      const list = track(() => listName);
+      console.log({ list });
+      newTodoInputState.editing = false;
+    });
+
+    return (
+      <div class="todo-list-card selected-list-wrapper">
+        <button
+          class="todo-list-card selected-list"
+          onClick$={() => {
+            if (!newTodoInputState.editing) {
+              newTodoInputState.editing = true;
+              newTodoInputState.value = listName;
+              newTodoInputState.valid = true;
+              newTodoInputState.dirty = false;
+            }
+          }}
+        >
+          {!newTodoInputState.editing && <span>{listName}</span>}
+          {newTodoInputState.editing && (
+            <input
+              ref={editRef}
+              type="text"
+              class={`selected-list-edit ${
+                newTodoInputState.valid ? "" : "invalid"
+              }`}
+              value={newTodoInputState.value ?? undefined}
+              onInput$={(event: any) => {
+                const value = event.target.value;
+                newTodoInputState.value = value;
+                newTodoInputState.dirty = true;
+                const trimmedValue = value.trim();
+                if (trimmedValue === listName) {
+                  newTodoInputState.valid = true;
+                  return;
+                }
+                if (
+                  !trimmedValue ||
+                  trimmedValue.length > 20 ||
+                  !!todoListsNames.includes(trimmedValue)
+                ) {
+                  newTodoInputState.valid = false;
+                  return;
+                }
+                newTodoInputState.valid = true;
+              }}
+              onBlur$={() => {
+                newTodoInputState.editing = false;
+              }}
+              onKeyUp$={async (event: any) => {
+                if (
+                  event.key === "Enter" &&
+                  newTodoInputState.dirty &&
+                  newTodoInputState.valid
+                ) {
+                  const newListName = newTodoInputState.value!.trim();
+                  const success = await onEditListName$(newListName);
+                  if (success) {
+                    newTodoInputState.editing = false;
+                  }
+                }
+              }}
+            />
+          )}
+        </button>
+        <button
+          class="btn delete-btn"
+          disabled={newTodoInputState.editing || todoListsNames.length <= 1}
+          onClick$={() => onDeleteList$()}
+        >
+          x
+        </button>
+      </div>
+    );
+  }
+);

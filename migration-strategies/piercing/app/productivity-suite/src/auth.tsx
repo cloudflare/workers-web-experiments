4b056249efac6f8b0e331995e311912a5628280f
Dario Piotrowicz <dario@cloudflare.com>
2022-10-21 23:13:35 +0100

    add pre-populated placeholder todo list

--- migration-strategies/piercing/app/productivity-suite/src/auth.tsx
+++ migration-strategies/piercing/app/productivity-suite/src/auth.tsx
@@ -6,6 +6,7 @@ import {
   getUserData,
   saveCurrentUser,
   setUserData,
+  TodoList,
 } from "shared";
 
 interface AuthContextType {
@@ -62,16 +63,33 @@ export function RequireNotAuth({ children }: { children: React.ReactNode }) {
   return <>{children}</>;
 }
 
+const placeholderTodoList: TodoList = {
+  name: "Important Tasks",
+  todos: [
+    {
+      text: "Create Cloudflare account",
+      completed: false,
+    },
+    {
+      text: "Experience Cloudflare workers",
+      completed: false,
+    },
+    {
+      text: "Read the Micro-Front-End Cloudflare blog posts",
+      completed: true,
+    },
+    {
+      text: "Help build a better internet",
+      completed: false,
+    },
+  ],
+};
+
 async function addUserDataIfMissing(user: string) {
   const data = getUserData(user);
   if (!data) {
     const newDataStr = JSON.stringify({
-      todoLists: [
-        {
-          name: "new list",
-          todos: [],
-        },
-      ],
+      todoLists: [placeholderTodoList],
     });
     setUserData(user, newDataStr);
   }

fc0aa823369404ec0b4bf9a2570595e0328713e3
Dario Piotrowicz <dario@cloudflare.com>
2022-10-24 14:09:07 +0100

    refactor ListOfTodos component

--- migration-strategies/piercing/app/fragments/todos/src/components/ListOfTodos.tsx
+++ migration-strategies/piercing/app/fragments/todos/src/components/ListOfTodos.tsx
@@ -16,7 +16,7 @@ export function ListOfTodos({
   onTodoRemoved: (todoText: string) => void;
 }) {
   const [editingTodoDetails, setEditingTodoDetails] = useState<{
-    oldTodoText: string;
+    oldTodo: Todo;
     newTodoText: string;
     invalid: boolean;
     dirty: boolean;
@@ -24,115 +24,133 @@ export function ListOfTodos({
 
   return (
     <ul className="todo-mvc-todos-list">
-      {todos.map(({ text, completed }) => (
-        <li
-          key={text}
-          className={`${completed ? "completed" : ""} ${
-            editingTodoDetails?.oldTodoText === text ? "editing" : ""
-          }`}
-        >
-          <div className="view">
-            <label className="hidden-label" htmlFor={`checkbox-toggle-${text}`}>
-              Toggle for {text}
-            </label>
-            <input
-              className="toggle"
-              type="checkbox"
-              id={`checkbox-toggle-${text}`}
-              checked={completed}
-              onChange={async (event: ChangeEvent<HTMLInputElement>) => {
-                const completed = event.target.checked;
-                const success = await editTodo(currentUser, listName, text, {
-                  text,
-                  completed,
-                });
-                if (success) {
-                  onTodoEdited(text, {
-                    text,
-                    completed,
-                  });
-                }
-              }}
-            />
-            <button
-              className="todo-text"
-              onClick={() =>
-                setTimeout(() =>
-                  setEditingTodoDetails({
-                    oldTodoText: text,
-                    newTodoText: text,
-                    invalid: false,
-                    dirty: false,
-                  })
-                )
-              }
-            >
-              {text}
-            </button>
-            <button
-              className="destroy"
-              onClick={async () => {
-                const success = await removeTodo(currentUser, listName, text);
-                if (success) {
-                  onTodoRemoved(text);
-                }
-              }}
-              aria-label="delete todo"
-            ></button>
-          </div>
-          {editingTodoDetails?.oldTodoText === text && (
-            <input
-              autoFocus
-              className={`edit ${editingTodoDetails.invalid ? "invalid" : ""}`}
-              value={editingTodoDetails.newTodoText}
-              onInput={(event: ChangeEvent<HTMLInputElement>) => {
-                const newTodoText = event.target.value;
-                const trimmedNewTodoText = newTodoText.trim();
-                const oldTodoText = editingTodoDetails.oldTodoText;
-                const invalid = !trimmedNewTodoText
-                  ? true
-                  : oldTodoText === trimmedNewTodoText
-                  ? false
-                  : !!todos.filter(({ text }) => text === trimmedNewTodoText)
-                      .length;
-                setEditingTodoDetails({
-                  oldTodoText,
-                  newTodoText,
-                  invalid,
-                  dirty: true,
-                });
-              }}
-              onKeyUp={async (event: React.KeyboardEvent<HTMLInputElement>) => {
-                if (
-                  event.key === "Enter" &&
-                  editingTodoDetails.dirty &&
-                  !editingTodoDetails.invalid
-                ) {
-                  const trimmedNewTodoText =
-                    editingTodoDetails.newTodoText.trim();
-                  const updatedTodo = {
-                    text: trimmedNewTodoText,
-                    completed,
-                  };
-                  const success = await editTodo(
-                    currentUser,
-                    listName,
-                    editingTodoDetails.oldTodoText,
-                    updatedTodo
-                  );
-                  if (success) {
-                    onTodoEdited(editingTodoDetails.oldTodoText, updatedTodo);
-                  }
-                  setEditingTodoDetails(null);
-                }
-              }}
-              onBlur={() => {
-                setEditingTodoDetails(null);
-              }}
-            />
-          )}
-        </li>
+      {todos.map((todo) => (
+        <TodoItem todo={todo} />
       ))}
     </ul>
   );
+
+  function TodoItem({ todo }: { todo: Todo }): JSX.Element {
+    async function handleCheckboxChange(event: ChangeEvent<HTMLInputElement>) {
+      const completed = event.target.checked;
+      const newTodoValue = { ...todo, completed };
+      const success = await editTodo(
+        currentUser,
+        listName,
+        todo.text,
+        newTodoValue
+      );
+      if (success) {
+        onTodoEdited(todo.text, newTodoValue);
+      }
+    }
+
+    function handleTodoTextClick(): void {
+      setTimeout(() =>
+        setEditingTodoDetails({
+          oldTodo: todo,
+          newTodoText: todo.text,
+          invalid: false,
+          dirty: false,
+        })
+      );
+    }
+
+    function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
+      const newTodoText = event.target.value;
+      const trimmedNewTodoText = newTodoText.trim();
+      const invalid = !trimmedNewTodoText
+        ? true
+        : editingTodoDetails!.oldTodo.text === trimmedNewTodoText
+        ? false
+        : !!todos.filter(({ text }) => text === trimmedNewTodoText).length;
+      setEditingTodoDetails({
+        oldTodo: editingTodoDetails!.oldTodo,
+        newTodoText,
+        invalid,
+        dirty: true,
+      });
+    }
+
+    async function editTodoTextIfValid() {
+      if (editingTodoDetails?.dirty && !editingTodoDetails?.invalid) {
+        const trimmedNewTodoText = editingTodoDetails.newTodoText.trim();
+        const updatedTodo = {
+          text: trimmedNewTodoText,
+          completed: editingTodoDetails.oldTodo.completed,
+        };
+        const success = await editTodo(
+          currentUser,
+          listName,
+          editingTodoDetails.oldTodo.text,
+          updatedTodo
+        );
+        if (success) {
+          onTodoEdited(editingTodoDetails.oldTodo.text, updatedTodo);
+        }
+        setEditingTodoDetails(null);
+      }
+    }
+
+    function handleInputKeyUp(event: React.KeyboardEvent<HTMLInputElement>) {
+      if (event.key === "Enter") {
+        editTodoTextIfValid();
+      }
+    }
+
+    function handleInputBlur() {
+      editTodoTextIfValid();
+      setEditingTodoDetails(null);
+    }
+
+    async function handleTodoDeletion() {
+      const success = await removeTodo(currentUser, listName, todo.text);
+      if (success) {
+        onTodoRemoved(todo.text);
+      }
+    }
+
+    return (
+      <li
+        key={todo.text}
+        className={`${todo.completed ? "completed" : ""} ${
+          editingTodoDetails?.oldTodo.text === todo.text ? "editing" : ""
+        }`}
+      >
+        <div className="view">
+          <label
+            className="hidden-label"
+            htmlFor={`checkbox-toggle-${todo.text}`}
+          >
+            Toggle for {todo.text}
+          </label>
+          <input
+            className="toggle"
+            type="checkbox"
+            id={`checkbox-toggle-${todo.text}`}
+            checked={todo.completed}
+            onChange={handleCheckboxChange}
+          />
+          <button className="todo-text" onClick={handleTodoTextClick}>
+            {todo.text}
+          </button>
+          <button
+            className="destroy"
+            onClick={handleTodoDeletion}
+            aria-label="delete todo"
+          ></button>
+        </div>
+        {editingTodoDetails?.oldTodo?.text === todo.text && (
+          <input
+            autoFocus
+            className={`edit ${editingTodoDetails.invalid ? "invalid" : ""}`}
+            value={editingTodoDetails.newTodoText}
+            onInput={handleInputChange}
+            onKeyUp={handleInputKeyUp}
+            onBlur={handleInputBlur}
+          />
+        )}
+      </li>
+    );
+  }
 }

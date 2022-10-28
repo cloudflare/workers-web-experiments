8786cc965f360632416050451e8702bdae807320
James Culveyhouse <jculveyhouse@cloudflare.com>
2022-10-28 13:36:57 -0500

    Remove user bits from api lib

--- productivity-suite/app/fragments/news/src/lib/api.ts
+++ productivity-suite/app/fragments/news/src/lib/api.ts
@@ -1,11 +1,9 @@
 import { isServer } from "solid-js/web";
 
 const story = (path: string) => `https://node-hnapi.herokuapp.com/${path}`;
-const user = (path: string) =>
-  `https://hacker-news.firebaseio.com/v0/${path}.json`;
 
 export default async function fetchAPI(path: string) {
-  const url = path.startsWith("user") ? user(path) : story(path);
+  const url = story(path);
   const headers = isServer ? { "User-Agent": "chrome" } : {};
 
   try {

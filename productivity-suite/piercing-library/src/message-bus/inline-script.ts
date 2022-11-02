eb7c66b265e288176aaa47ca6ae7ae4c7082f72e
Dario Piotrowicz <dario@cloudflare.com>
2022-11-02 11:04:57 +0000

    remove unused message bus and simplify message bus inline script

--- productivity-suite/piercing-library/src/message-bus/inline-script.ts
+++ productivity-suite/piercing-library/src/message-bus/inline-script.ts
@@ -1,4 +1,9 @@
-import { createBrowserMessageBusFromStateStr } from "./browser-message-bus";
+import { GenericMessageBus } from "./message-bus";
+import { messageBusProp } from "./message-bus-prop";
 
 declare const __MESSAGE_BUS_STATE_STR_PLACEHOLDER__: string;
-createBrowserMessageBusFromStateStr(__MESSAGE_BUS_STATE_STR_PLACEHOLDER__);
+
+const state = JSON.parse(__MESSAGE_BUS_STATE_STR_PLACEHOLDER__ ?? "{}");
+(globalThis as any as { [messageBusProp]?: GenericMessageBus })[
+  messageBusProp
+] = new GenericMessageBus(state);

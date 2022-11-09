a57cfc80342a3dcf73576d57ddf31f6eb7368057
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2022-11-09 20:11:54 +0000

    Productivity Suite - add 404 page (#63)
    add 404 page

--- productivity-suite/app/legacy-app/src/components/pages/Contacts.tsx
+++ productivity-suite/app/legacy-app/src/components/pages/Contacts.tsx
@@ -1,4 +1,4 @@
-import { UnimplementedDemoPage } from "../UnimplementedDemoPage";
+import { UnimplementedDemoPage } from "./UnimplementedDemoPage";
 
 const contactsSvg = (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">

7f07b34e41d86a0f41c45755552da4bcbf0f51a9
Dario Piotrowicz <dario@cloudflare.com>
2022-11-14 18:23:01 +0000

    chore(productivity-suite): remove unneede jsx fragment

--- productivity-suite/app/fragments/login/src/root.tsx
+++ productivity-suite/app/fragments/login/src/root.tsx
@@ -2,9 +2,5 @@ import { component$ } from "@builder.io/qwik";
 import { LoginForm } from "./components/LoginForm";
 
 export const Root = component$(() => {
-  return (
-    <>
-      <LoginForm />
-    </>
-  );
+  return <LoginForm />;
 });

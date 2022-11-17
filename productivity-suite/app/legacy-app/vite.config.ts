5d66b70f85edcbf90d2b012406e82fd3b91714d2
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2022-11-17 15:58:59 +0000

    fix(productivity-suite): apply cloudflare web analytics only on prod (#87)
    * fix(productivity-suite): apply cloudflare web analytics only on production builds
    
    When running locally in dev-mode, we get CORS errors because we are including the
    cloudflare web-analytics scripts. Prevent such behavior by only applying the web
    analytics scripts to the production builds.

--- productivity-suite/app/legacy-app/vite.config.ts
+++ productivity-suite/app/legacy-app/vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from "vite";
+import { defineConfig, HtmlTagDescriptor, PluginOption } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), addCloudflareAnalyticsScript()],
   server: {
     cors: true,
     port: 8585,
@@ -13,3 +13,23 @@ export default defineConfig({
     },
   },
 });
+
+export function addCloudflareAnalyticsScript(): PluginOption {
+  return {
+    name: "add-cloudflare-analytics-script",
+    apply: "build",
+    transformIndexHtml() {
+      const tags: HtmlTagDescriptor[] = [
+        {
+          tag: "script",
+          attrs: {
+            defer: true,
+            src: "https://static.cloudflareinsights.com/beacon.min.js",
+            "data-cf-beacon": "{'token': '43a08321febf43fe9103890be2f3bb5f'}",
+          },
+        },
+      ];
+      return tags;
+    },
+  };
+}

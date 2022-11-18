48c2504633fe225e08129cb9f1f4df25c28e0371
Pete Bacon Darwin <pete@bacondarwin.com>
2022-11-18 16:17:11 +0000

    refactor(productivity-suite): update SolidJS version to avoid global leak workaround (#91)
    Previously, SolidJS (used in the news fragment) was leaving data on the global
    object, which broke the news component when the application tried to re-render
    it after navigating away.
    
    Updating to the latest SolidJS version 1.6.2 fixes this problem, so we can
    remove the workaround.

--- productivity-suite/app/legacy-app/src/components/pages/News.tsx
+++ productivity-suite/app/legacy-app/src/components/pages/News.tsx
@@ -1,16 +1,4 @@
-import { useEffect } from "react";
-
 export function News() {
-  useEffect(() => {
-    return () => {
-      // @ts-ignore
-      window.e = window._$HY = null;
-
-      // @ts-ignore
-      window.t = function () {};
-    };
-  });
-
   return (
     <div>
       <piercing-fragment-outlet fragment-id="news" />

68feeb251efaf1eccc0892e63302fc50e8ac79e5
Pete Bacon Darwin <pete@bacondarwin.com>
2022-11-28 09:56:05 +0000

    refactor(productivity-suite): create a new global MessageBus if there is none
    Previously we threw an error if there was no global message bus defined.
    But this is a bit too strict for scenarios like running the legacy-app separately
    from the gateway worker.
    Now we just log a warning and create a blank bus as needed.

--- productivity-suite/piercing-library/src/message-bus/get-bus.ts
+++ productivity-suite/piercing-library/src/message-bus/get-bus.ts
@@ -1,13 +1,17 @@
 import { getFragmentHost } from "../piercing-fragment-host/get-fragment-host";
-import type { MessageBus } from "./message-bus";
+import { GenericMessageBus, type MessageBus } from "./message-bus";
 import { messageBusProp } from "./message-bus-prop";
 
 /**
- * Gets a message bus based on the provided element.
+ * Get a `MessageBus` based on the provided element.
  *
- * @param element Element from where to start looking for a message bus (up the DOM tree)
- *                if there is no intermediate message bus or the element is not provided
- *                the message bus in the global scope is returned.
+ * Search up the ancestors of the given `element` looking for a `FragmentHost`.
+ * If there is one then return the `MessageBus` attached to that.
+ *
+ * If there is no `FragmentHost` ancestor or `element` is not provided then
+ * the global scope `MessageBus` is returned.
+ *
+ * @param element Element from where to start looking for a message bus (up the DOM tree).
  * @returns       The most relevant message bus.
  */
 export function getBus(element?: Element): MessageBus {
@@ -21,9 +25,16 @@ export function getBus(element?: Element): MessageBus {
   if (hasMessageBus(root)) {
     return root[messageBusProp];
   }
-  throw new Error("No global message bus defined!");
+  console.warn("No global message bus defined - creating an empty one.");
+
+  return ((globalThis as unknown as { [messageBusProp]?: GenericMessageBus })[
+    messageBusProp
+  ] = new GenericMessageBus());
 }
 
+/**
+ * Is the given `obj` a `MessageBus` container?
+ */
 function hasMessageBus<T>(obj: T): obj is T & { [messageBusProp]: MessageBus } {
   return !!obj && messageBusProp in obj;
 }

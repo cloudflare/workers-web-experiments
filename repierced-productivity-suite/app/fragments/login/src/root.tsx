c6e797570f7b8d7eb0f5b945e6be07778c07c36d
James Culveyhouse <jculveyhouse@cloudflare.com>
2022-12-12 10:51:57 -0600

    Copying productivity-suite

---
+++ repierced-productivity-suite/app/fragments/login/src/root.tsx
@@ -0,0 +1,6 @@
+import { component$ } from "@builder.io/qwik";
+import { LoginForm } from "./components/LoginForm";
+
+export const Root = component$(() => {
+  return <LoginForm />;
+});
---
+++ repierced-productivity-suite/piercing-library/piercing-fragment-host.vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import path from "node:path";
+
+export default defineConfig({
+  build: {
+    outDir: "dist",
+    emptyOutDir: false,
+    lib: {
+      entry: path.resolve(
+        __dirname,
+        `src/piercing-fragment-host/inline-script.ts`
+      ),
+      formats: ["es"],
+      fileName: "piercing-fragment-host-inline-script",
+    },
+  },
+});

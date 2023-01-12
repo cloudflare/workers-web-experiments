21c4a35c544135d5439a7a9b7181154a0bf376fc
Dario Piotrowicz <dario@cloudflare.com>
2023-01-12 23:08:35 +0000

    add server actions to QwikCity todo app

--- d1-todos/apps/qwik-city/src/routes/index.tsx
+++ d1-todos/apps/qwik-city/src/routes/index.tsx
@@ -1,11 +1,10 @@
 import {
   component$,
-  Resource,
   useClientEffect$,
   useSignal,
-  useStore,
+  useTask$,
 } from "@builder.io/qwik";
-import { RequestHandler, useEndpoint } from "@builder.io/qwik-city";
+import { loader$, action$, Form } from "@builder.io/qwik-city";
 import {
   type Todo,
   getTodos,
@@ -13,19 +12,23 @@ import {
   editTodo,
   deleteTodo,
   validateTodoText,
+  saveSessionIdCookie,
   getOrCreateSessionId,
   getSessionIdFromRequest,
-  saveSessionIdCookie,
 } from "shared";
 
 interface Platform {
   D1_TODOS_DB: D1Database;
 }
 
-export const onGet: RequestHandler<{ todos: Todo[] }, Platform> = async ({
-  request,
-  platform,
-}) => {
+export const dataLoader = loader$<
+  Platform,
+  Promise<{
+    sessionId: string;
+    todos: Todo[];
+    error?: string;
+  }>
+>(async ({ request, platform }) => {
   const todosDb = getTodosDb(platform);
   const sessionId = await getOrCreateSessionId(request as Request, todosDb);
   const todos = await getTodos(todosDb, sessionId);
@@ -33,178 +36,155 @@ export const onGet: RequestHandler<{ todos: Todo[] }, Platform> = async ({
     sessionId,
     todos,
   };
-};
+});
 
-export const onPost: RequestHandler<
-  { todos: Todo[]; error?: string },
-  Platform
-> = async ({ request, platform }) => {
+export const addAction = action$(async (formData, { request, platform }) => {
+  const todosDb = getTodosDb(platform as Platform);
+  const timestamp = new Date().getTime();
   const sessionId = getSessionIdFromRequest(request as Request);
-
-  const formData = await request.formData();
-  const operation = formData.get("operation") as "add" | "edit" | "delete";
-  let error: string | null = null;
-
-  let todos: Todo[] = [];
-  const db = getTodosDb(platform);
-
   if (!sessionId) {
-    error = "Session id not provided";
-  } else {
-    try {
-      const operationFn = operationFnsMap[operation];
-      await operationFn(formData, db, sessionId);
-      todos = await getTodos(db, sessionId);
-    } catch (e: any) {
-      error = e.message;
-    }
+    return { error: "Session Id not defined!" };
   }
-
-  return {
-    todos,
-    ...(error ? { error } : {}),
-  };
-};
-
-const operationFnsMap = {
-  add: handleTodoAdd,
-  edit: handleTodoEdit,
-  delete: handleTodoDelete,
-} as const;
-
-async function handleTodoAdd(
-  formData: FormData,
-  db: D1Database,
-  sessionId: string
-): Promise<void> {
   const text = formData.get("text") as string;
   const checkResult = validateTodoText(text);
   if (checkResult.valid) {
     try {
-      await addTodo(db, sessionId, text);
+      await addTodo(todosDb, sessionId, text);
     } catch {
-      throw new Error("DataBase Internal Error");
+      return { error: "DataBase Internal Error", timestamp };
     }
   } else {
-    throw new Error(checkResult.reason!);
+    return { error: checkResult.reason!, timestamp };
   }
-}
+  return { addedTodo: text, timestamp };
+});
 
-async function handleTodoEdit(
-  formData: FormData,
-  db: D1Database,
-  sessionId: string
-): Promise<void> {
+export const editAction = action$(async (formData, { request, platform }) => {
+  const todosDb = getTodosDb(platform as Platform);
+  const sessionId = getSessionIdFromRequest(request as Request);
+  const timestamp = new Date().getTime();
+
+  if (!sessionId) {
+    return { error: "Session Id not defined!", timestamp };
+  }
   const id = formData.get("todo-id") as string;
   const completed = (formData.get("completed") as string) === "true";
   try {
-    await editTodo(db, sessionId, id, completed);
+    await editTodo(todosDb, sessionId, id, completed);
   } catch {
-    throw new Error("DataBase Internal Error");
+    return { error: "DataBase Internal Error", timestamp };
   }
-}
 
-async function handleTodoDelete(
-  formData: FormData,
-  db: D1Database,
-  sessionId: string
-): Promise<void> {
+  return { timestamp };
+});
+
+export const deleteAction = action$(async (formData, { request, platform }) => {
+  const todosDb = getTodosDb(platform as Platform);
+  const sessionId = getSessionIdFromRequest(request as Request);
+  const timestamp = new Date().getTime();
+
+  if (!sessionId) {
+    return { error: "Session Id not defined!", timestamp };
+  }
   const id = formData.get("todo-id") as string;
   try {
-    await deleteTodo(db, sessionId, id);
+    await deleteTodo(todosDb, sessionId, id);
   } catch {
-    throw new Error("DataBase Internal Error");
+    return { error: "DataBase Internal Error", timestamp };
   }
-}
+
+  return { timestamp };
+});
 
 export default component$(() => {
-  const data = useEndpoint<{
-    sessionId: string;
-    todos: Todo[];
-    error?: string;
-  }>();
+  const data = dataLoader.use();
+  const { sessionId, todos } = data.value;
 
   const newTodo = useSignal<string>("");
-  const sessionState = useStore({ sessionId: "" });
+  const error = useSignal<string | undefined>();
 
-  useClientEffect$(({ track }) => {
-    track(sessionState);
+  useClientEffect$(() => {
+    if (sessionId) {
+      saveSessionIdCookie(sessionId);
+    }
+  });
 
-    if (sessionState.sessionId) {
-      saveSessionIdCookie(sessionState.sessionId);
+  const addActionUtils = addAction.use();
+  const editActionUtils = editAction.use();
+  const deleteActionUtils = deleteAction.use();
+
+  useTask$(({ track }) => {
+    track(() => addActionUtils.value);
+    track(() => editActionUtils.value);
+    track(() => deleteActionUtils.value);
+
+    const latestActionUtilsValue = [
+      addActionUtils.value,
+      editActionUtils.value,
+      deleteActionUtils.value,
+    ].reduce((acc, curr) =>
+      (curr?.timestamp ?? 0) > (acc?.timestamp ?? 0) ? curr : acc
+    );
+
+    const latestError = latestActionUtilsValue?.error;
+
+    error.value = latestError;
+
+    if (!latestError && latestActionUtilsValue === addActionUtils.value) {
+      newTodo.value = "";
     }
   });
 
   return (
-    <Resource
-      value={data}
-      onPending={() => <div></div>}
-      onRejected={() => <div></div>}
-      onResolved={({ sessionId, todos, error }) => {
-        if (sessionId) {
-          sessionState.sessionId = sessionId;
-        }
-
-        return (
-          <div class="todos-app">
-            <h1>Todos</h1>
-            <form method="post" class="add-todo-wrapper">
-              <div class="new-todo-input-form-control">
-                <input hidden name="operation" readOnly value="add" />
-                <input
-                  class={
-                    !newTodo.value || validateTodoText(newTodo.value).valid
-                      ? ""
-                      : "invalid"
-                  }
-                  type="text"
-                  name="text"
-                  value={newTodo.value}
-                  onInput$={(event) => {
-                    newTodo.value = (
-                      event.target as unknown as { value: string }
-                    ).value;
-                  }}
-                />
-                <button disabled={!newTodo.value}>add todo</button>
-              </div>
-            </form>
-            {error && <p class="backend-error">{error}</p>}
-            <ul>
-              {todos.map(({ id, text, completed }) => (
-                <li key={id}>
-                  <form method="post">
-                    <input hidden name="operation" readOnly value="edit" />
-                    <input hidden name="todo-id" readOnly value={id} />
-                    <input
-                      hidden
-                      name="completed"
-                      readOnly
-                      value={`${!completed}`}
-                    />
-                    <button
-                      role="checkbox"
-                      aria-checked={completed}
-                      aria-label={text}
-                      class={`custom-checkbox ${completed ? "checked" : ""}`}
-                    ></button>
-                  </form>
-                  <span class={completed ? "line-through" : ""}>{text}</span>
-                  <form method="post">
-                    <input hidden name="operation" readOnly value="delete" />
-                    <input hidden name="todo-id" readOnly value={id} />
-                    <button class="delete-btn" aria-label="delete"></button>
-                  </form>
-                </li>
-              ))}
-            </ul>
-          </div>
-        );
-      }}
-    />
+    <div class="todos-app">
+      <h1>Todos</h1>
+      <Form action={addActionUtils} class="add-todo-wrapper">
+        <div class="new-todo-input-form-control">
+          <input hidden name="operation" readOnly value="add" />
+          <input
+            class={
+              !newTodo.value || validateTodoText(newTodo.value).valid
+                ? ""
+                : "invalid"
+            }
+            type="text"
+            name="text"
+            value={newTodo.value}
+            onInput$={(event) => {
+              newTodo.value = (
+                event.target as unknown as { value: string }
+              ).value;
+            }}
+          />
+          <button disabled={!newTodo.value}>add todo</button>
+        </div>
+      </Form>
+      {error.value && <p class="backend-error">{error.value}</p>}
+      <ul>
+        {todos.map(({ id, text, completed }) => (
+          <li key={id}>
+            <Form action={editActionUtils}>
+              <input hidden name="todo-id" readOnly value={id} />
+              <input hidden name="completed" readOnly value={`${!completed}`} />
+              <button
+                role="checkbox"
+                aria-checked={completed}
+                aria-label={text}
+                class={`custom-checkbox ${completed ? "checked" : ""}`}
+              ></button>
+            </Form>
+            <span class={completed ? "line-through" : ""}>{text}</span>
+            <Form action={deleteActionUtils}>
+              <input hidden name="todo-id" readOnly value={id} />
+              <button class="delete-btn" aria-label="delete"></button>
+            </Form>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 });
 
-function getTodosDb(platform: Platform): D1Database {
+export function getTodosDb(platform: Platform): D1Database {
   return platform["D1_TODOS_DB"] as D1Database;
 }
